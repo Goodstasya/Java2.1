80ab44699a9a4758d0bea48f9f50ea2d6712e30f
Ilina Anastasiia <goodstasya@gmail.com>
2020-07-02 14:29:59 +0200

    All

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        float cost = 3655;
+        int bonus = 20;
+
+        float total = cost / bonus;
+        System.out.println(total);
+    }
+}
